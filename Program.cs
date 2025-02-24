e6b6b99687f1715e4dd84fdd0388a3c5176f5941
Vikas Brungi <Vikas_Brungi@uhaul.com>
2025-02-24 07:21:29 -0700

    Working model for full workflow Added filter to Create tasks only Individual tasks.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -13,11 +19,12 @@ class Program
 
     static async Task Main(string[] args)
     {
-        string parentWorkItemId = "1287264"; // Replace with the actual WR ID
+        string parentWorkItemId = "1353003"; // Replace with the actual WR ID
         //await CopyWRToTask(parentWorkItemId);
 
         var missingWRs = await GetMissingTasksFromQuery();
         await CreateTasksForMissingWorkItems(missingWRs);
+        //await CopyWRToTask("1324455");
     }
 
     static async Task CreateTasksForMissingWorkItems(List<int>? missingWRs)
@@ -28,31 +35,67 @@ class Program
            // var workItemDetails = await GetWorkItemDetails(workItemId);
 
             // Create a task for the current work item
-            await CopyWRToTask(workItemId.ToString());
+            await CreatetasksFromWR(workItemId.ToString());
         }
     }
     static async Task<List<int>> GetMissingTasksFromQuery()
     {
-        var queryUrl = "https://dev.azure.com/uhaul/U-Haul%20IT/_apis/wit/queries/068b5dbc-6545-4bf5-82b7-9c3e6522a643?api-version=6.0";  // The URL to the query API
-        var client = new HttpClient();
-        client.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(":" + pat))); 
+        string collectionUrl = "https://dev.azure.com/uhaul/"; 
+        string projectName = "U-Haul IT";
 
-        HttpResponseMessage response = await client.GetAsync(queryUrl);
-        response.EnsureSuccessStatusCode();
+        //var queryUrl = "https://dev.azure.com/uhaul/U-Haul%20IT/_apis/wit/wiql/068b5dbc-6545-4bf5-82b7-9c3e6522a643?api-version=7.1-preview.3";  // The URL to the query API
+        //var client = new HttpClient();
+        //client.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(":" + pat))); 
 
-        var responseContent = await response.Content.ReadAsStringAsync();
+        //HttpResponseMessage response = await client.GetAsync(queryUrl);
+        //response.EnsureSuccessStatusCode();
 
-        //using (JsonDocument doc = JsonDocument.Parse(responseContent))
-        //{
-        //    return doc.RootElement;
-        //}
-        var jsonResponse = JObject.Parse(responseContent);
-        var workItems = jsonResponse["workItems"].Select(wi => (int)wi["id"]).ToList();  // Extract work item ids
+        //var responseContent = await response.Content.ReadAsStringAsync();
 
-        return workItems;
+        ////using (JsonDocument doc = JsonDocument.Parse(responseContent))
+        ////{
+        ////    return doc.RootElement;
+        ////}
+        //var jsonResponse = JObject.Parse(responseContent);
+        //var workItems = jsonResponse["workItems"].Select(wi => (int)wi["id"]).ToList();  // Extract work item ids
+
+        //return workItems;
+
+        VssConnection connection = new VssConnection(new Uri(orgUrl), new VssBasicCredential(string.Empty, pat));
+        var workItemTrackingHttpClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+        string wiqlQuery = $@"
+            SELECT
+                [System.Id]
+            FROM workitemLinks
+            WHERE
+                (
+                    [Source].[System.TeamProject] = '{project}'
+                    AND [Source].[System.IterationPath] = @currentIteration('[U-Haul IT]\uhaul.com Development <id:bf28a9bf-fe86-431b-997b-b3ccd170156a>')
+                    AND [Source].[System.WorkItemType] IN ('Work Request', 'Bug')
+                    AND (
+                        [Source].[System.AreaPath] UNDER 'U-Haul IT\Chris Jestice\Neal Valiant'
+                        OR [Source].[System.AreaPath] UNDER 'U-Haul IT\Chris Jestice\Uhaul.com'
+                    )
+                    AND [Source].[System.State] <> 'Closed'
+                    AND NOT [Source].[System.Title] CONTAINS 'Regression'
+                )
+                AND ([System.Links.LinkType] = 'System.LinkTypes.Hierarchy-Forward')
+                AND ([Target].[System.TeamProject] = '{project}'
+                    AND [Target].[System.WorkItemType] = 'Task')
+                MODE (DoesNotContain)";
+
+        // Execute the query
+        var wiql = new Wiql() { Query = wiqlQuery };
+        var workItemLinks = await workItemTrackingHttpClient.QueryByWiqlAsync(wiql);
+
+        // Extract the work item IDs
+        var workItemIds = workItemLinks.WorkItems.Select(wi => wi.Id).ToList();
+        return workItemIds;
+
     }
 
-    static async Task CopyWRToTask(string parentId)
+    static async Task CreatetasksFromWR(string parentId, bool onlyYourTasks=true)
     {
         using (HttpClient client = new HttpClient())
         {
@@ -73,6 +116,13 @@ class Program
             using JsonDocument doc = JsonDocument.Parse(wrResponseBody);
             string title = doc.RootElement.GetProperty("fields").GetProperty("System.Title").GetString();
             string description = doc.RootElement.GetProperty("fields").GetProperty("System.Description").GetString();
+            string assignedTo = doc.RootElement.GetProperty("fields").GetProperty("System.AssignedTo").GetProperty("displayName").GetString();
+            string effort = doc.RootElement.GetProperty("fields").GetProperty("Microsoft.VSTS.Scheduling.Effort").GetDouble().ToString();
+
+            if(assignedTo?.ToUpper() != "VIKAS BRUNGI")
+            {
+                return;
+            }
 
             // Extract AreaPath and IterationPath
             string areaPath = doc.RootElement.GetProperty("fields").GetProperty("System.AreaPath").GetString();
@@ -90,9 +140,11 @@ class Program
 [
     {{ ""op"": ""add"", ""path"": ""/fields/System.Title"", ""value"": ""{title}"" }},
     {{ ""op"": ""add"", ""path"": ""/fields/System.Description"", ""value"": ""{EscapeJsonString(description)}"" }},
-    {{ ""op"": ""add"", ""path"": ""/fields/Microsoft.VSTS.Common.ItemDescription"", ""value"": ""Copied from WR #{parentId}"" }},
+    {{ ""op"": ""add"", ""path"": ""/fields/Microsoft.VSTS.Common.ItemDescription"", ""value"": ""{EscapeJsonString(description)}"" }},
     {{ ""op"": ""add"", ""path"": ""/fields/System.AreaPath"", ""value"": ""{EscapeJsonString(areaPath)}"" }},
     {{ ""op"": ""add"", ""path"": ""/fields/System.IterationPath"", ""value"": ""{EscapeJsonString(iterationPath)}"" }},
+    {{ ""op"": ""add"", ""path"": ""/fields/System.AssignedTo"", ""value"": ""{assignedTo}"" }},
+    {{ ""op"": ""add"", ""path"": ""/fields/Microsoft.VSTS.Scheduling.Effort"", ""value"": {effort} }},
     {{ ""op"": ""add"", ""path"": ""/relations/-"", ""value"": 
                     {{ ""rel"": ""System.LinkTypes.Hierarchy-Reverse"", 
                        ""url"": ""{orgUrl}/{project}/_apis/wit/workItems/{parentId}"" }} 
